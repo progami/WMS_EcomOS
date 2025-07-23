96c25c30c44f5ef03685520c5e485717cf2288ec
jarrar <jarrar@targonglobal.com>
2025-07-23 11:42:38 -0500

    chore: Clean up unnecessary files and improve .gitignore
    - Remove obsolete deployment scripts and documentation
    - Remove test artifacts and HAR files (11MB performance-har)
    - Remove unused test scripts and configs
    - Update .gitignore to properly exclude test artifacts
    - Add .repomixignore to exclude large files from repomix output
    - This reduces repomix token count from 6M to manageable size

--- scripts/test-server.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-// Set test authentication environment variable
-process.env.USE_TEST_AUTH = 'true'
-process.env.NODE_ENV = 'test'
-
-// Start the regular server
-require('../server.js')
